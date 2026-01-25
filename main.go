0b4bd21ecc82fa35cdd252771d9d9e9bf2168113
pnolann <nolann.pierreantoine@ynov.com>
2026-01-25 16:42:46 +0100

    fix(main): main file fixed

--- main.go
+++ main.go
@@ -8,18 +8,14 @@ import (
 )
 
 func main() {
-	// Fichiers statiques
+	// Statics Files
 	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.Handle("/static/", http.StripPrefix("/static", fs))
 
 	// Routes
 	http.HandleFunc("/", handlers.HomeHandler)
 	http.HandleFunc("/artists", handlers.ArtistsHandler)
 
 	fmt.Println("Server is running on http://localhost:8080")
-
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
