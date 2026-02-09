3b5186ac4e10d1cc1ae8afe83cf5b2baf950d932
pnolann <nolann.pierreantoine@ynov.com>
2026-02-09 22:07:22 +0100

    clear(files): delete some files un use

--- models/dates.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type Dates struct{
-	Id int `json:"id"`
-	Dates []string `json:"dates"`
-}
--- models/locations.go
+++
@@ -1,8 +0,0 @@
-
-package models 
-
-type Locations struct {
-    Id int `json:"id"`
-    Locations []string `json:"locations"`
-    Dates string `json:"dates"`
-}
